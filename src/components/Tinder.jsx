8fda8ba3df3bd1ee1820482de6f67d6be01874cd
Oscar Carrion <oscar.carrion@mide.org.mx>
2022-03-01 19:18:22 -0600

    finished tinder card implementation

--- src/components/Tinder.jsx
+++ src/components/Tinder.jsx
@@ -1,65 +1,131 @@
 import TinderCard from "react-tinder-card"
 import { useState } from "react"
+import data from "./monsterData"
+import './Tinder.css'
 
 const Tinder = () => {
-    const [monstruos, setMonstruos] = useState([
-        {
-            nombre: 'deudas'
-        },
-        {
-            nombre: 'compras'
-        },
-        {
-            nombre: 'impostor'
-        },
-        {
-            nombre: 'meses'
-        },
-        {
-            nombre: 'saboteador'
-        },
-        {
-            nombre: 'credito'
-        },
-        {
-            nombre: 'imprevistos'
-        },
-        {
-            nombre: 'hormiga'
-        },
-        {
-            nombre: 'ultimo'
-        },
-    ])
+    const [monstruos, setMonstruos] = useState(data)
+    const [finished, setFinished] = useState(false)
+    const [high, setHigh] = useState(0)
+    const [name, setName] = useState('')
 
-    const onSwipe = (direction, nombre) => {
-        console.log(direction, nombre)
+    const getHighest = () => {
+        let temp = 0
+        let tempName = ''
+        monstruos.forEach(monstruo => {
+            if(temp < monstruo.score) {
+                console.log('score is', monstruo.score)
+                temp = monstruo.score
+                tempName = monstruo.nombre
+            }
+        })
+        setHigh(temp)
+        setName(tempName)
+    }
+
+    const updateMonstruo = (index, score) => {
+        let newArr = [...monstruos]
+        newArr[index].score = newArr[index].score + score
+        setMonstruos(newArr)
+    }
+
+    const onSwipe = (direction, nombre, index) => {
         switch (nombre) {
             case 'deudas':
                 if(direction === 'left') {
-                    console.log('2 pt')
-                } else {
-                    console.log('2 pt deudas')
+                    updateMonstruo(index, 2)
+                    setFinished(true)
+                    getHighest()
+                } else if(direction === 'right'){
+                    updateMonstruo(index, 2)
+                    setFinished(true)
+                    getHighest()
                 }
+                break
             case 'compras':
                 if(direction === 'left') {
-                    console.log('something')
-                } else {
-                    console.log('no')
+                    updateMonstruo(index, 3)
+                } else if(direction === 'right'){
+                    updateMonstruo(5, 2)
                 }
+                break
+            case 'impostor':
+                if(direction === 'left') {
+                    updateMonstruo(index, 5)
+                } else if(direction === 'right') {
+                    updateMonstruo(6, 2)
+                }
+                break
+            case 'meses':
+                if(direction === 'left') {
+                    updateMonstruo(index, 3)
+                } else if(direction === 'right') {
+                    updateMonstruo(6, 2)
+                }
+                break
+            case 'saboteador':
+                if(direction === 'left') {
+                    updateMonstruo(index, 2)
+                } else if(direction === 'right') {
+                    updateMonstruo(3, 2)
+                }
+                break
+            case 'credito':
+                if(direction === 'left') {
+                    updateMonstruo(index, 2)
+                } else if(direction === 'right') {
+                    updateMonstruo(0, 1)
+                }
+                break
+            case 'imprevistos':
+                if(direction === 'left') {
+                    updateMonstruo(index, 1)
+                } else if(direction === 'right') {
+                    updateMonstruo(1, 2)
+                }
+                break
+            case 'hormiga':
+                if(direction === 'left') {
+                    updateMonstruo(index, 5)
+                } else if(direction === 'right') {
+                    updateMonstruo(4, 3)
+                }
+                break
+            case 'ultimo':
+                if(direction === 'left') {
+                    updateMonstruo(4, 5)
+                } else if(direction === 'right') {
+                    updateMonstruo(5, 1)
+                }
+                break
         }
     }
+
     return(
         <div className="d-flex justify-content-center">
-            <div className="d-flex flex-column">
-                {monstruos.map(monstruo =>     
+            <div className="d-flex flex-row justify-content-center mt-3">
+                {data.map((monstruo, index) =>     
                     <TinderCard 
+                        className="swipe"
                         key={monstruo.nombre} 
-                        onSwipe={(direction) => onSwipe(direction, monstruo.nombre)}
+                        onSwipe={(direction) => onSwipe(direction, monstruo.nombre, index)}
+                        preventSwipe={['up','down']}
                     >
-                        <div className="card">{monstruo.nombre}</div>
+                        <div style={{ backgroundImage: `url(${monstruo.url})`}} className="tinderCard">
+                            <h3 className="p-3">{monstruo.nombre}</h3>
+                            <div className="card-text">
+                                <span>{monstruo.pregunta}</span>
+                            </div>
+                            <div className="swiping">
+                                <span className="left">{monstruo.left}</span>
+                                <span className="right">{monstruo.right}</span>
+                            </div>
+                        </div>
                     </TinderCard>
                 )}
+                {finished &&
+                    <h1>Tu monstruo es {name}</h1>
+                }
             </div>
         </div>
     )
---
+++ src/components/monsterData.js
@@ -0,0 +1,62 @@
+ const data = [
+    {
+        nombre: 'deudas',
+        url: 'https://picsum.photos/500',
+        pregunta: 'Tu deuda tiene dos pagos pendientes pero este fin hay venta nocturna, tú: ',
+        left: 'Aprovechas a comprar la freidora de aire que tanto querías.',
+        right: 'Renuevas tu guardarropa porque hay muchas ofertas.', 
+        score: 0
+    },
+    {
+        nombre: 'compras',
+        url: 'https://picsum.photos/500',
+        pregunta: 'Revisando tus redes sociales, observas el anuncio de unos tenis de edición limitada que no tenías planeado comprar, tú:',
+        score: 0
+    },
+    {
+        nombre: 'impostor',
+        url: 'https://picsum.photos/500',
+        pregunta: 'Te hablan de un número desconocido para decirte que tienes un cargo no reconocido en tu tarjeta, tú:',
+        score: 0
+    },
+    {
+        nombre: 'meses',
+        url: 'https://picsum.photos/500',
+        pregunta: 'Es navidad y decides comprar regalos para toda tu familia, tú:',
+        score: 0
+    },
+    {
+        nombre: 'saboteador',
+        url: 'https://picsum.photos/500',
+        pregunta: 'Tomaste un curso de repostería y te gustó tanto que piensas emprender un negocio, tú:',
+        score: 0
+    },
+    {
+        nombre: 'credito',
+        url: 'https://picsum.photos/500',
+        pregunta: 'Un día vas con tus amigos al centro comercial, tú:',
+        score: 0
+    },
+    {
+        nombre: 'imprevistos',
+        url: 'https://picsum.photos/500',
+        pregunta: 'Estás tomando clases virtuales o en home office y por accidente derramas café en tu computadora, tú:',
+        score: 0
+    },
+    {
+        nombre: 'hormiga',
+        url: 'https://picsum.photos/500',
+        pregunta: 'El trayecto de tu casa a tu trabajo o escuela es un poco largo, tú: ',
+        score: 0
+    },
+    {
+        nombre: 'ultimo',
+        url: 'https://picsum.photos/500',
+        pregunta: 'Ves en redes sociales que está moda un opción de invesión/negocio que genera grandes ganancias, solo necesitas invertir poco dinero e invitar a otros amigos para que se unan, tú:',
+        left: 'Haces una transferencia para participar en el negocio ya que quieres dinero rápido e invitas a tres de tus conocidos',
+        right: 'Rechazas la oferta, pero pides un crédito para poner tu propio negocio aunque no sabes cómo iniciar.',
+        score: 0
+    },
+ ]
+
+ export default data
